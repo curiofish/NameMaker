1c2b2ad5a9fed8d73b5b567838509bc56f9dddec
curiofish <fishcurio@gmail.com>
2025-03-07 19:40:13 +0900

    Update server configuration for better stability

--- wsgi.py
+++ wsgi.py
@@ -7,4 +7,5 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from app import app
 
 if __name__ == "__main__":
-    app.run() 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port) 
