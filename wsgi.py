80b51f73c2ab305dc8288bacc757981e33b476c3
curiofish <fishcurio@gmail.com>
2025-03-07 19:40:48 +0900

    Update server configuration for better port binding

--- wsgi.py
+++ wsgi.py
@@ -7,5 +7,5 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from app import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port) 
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=False) 
